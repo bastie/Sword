3251be806677d70efbfb9b362dea7d0a9d4f11e1
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2022-03-25 16:03:57 +0100

    I did not copy this code except to IOFacade.

---
+++ Sources/KnightLife/io/IOFacade.swift
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2022 - Sebastian Ritter <bastie@users.noreply.github.com>
+ * SPDX-License-Identifier: MIT
+ */
+import Foundation
+
+///
+/// IOFacade
+///
+struct IOFacade {
+    
+    private static let fileManagerInstance = FileManager()
+    
+    static public func exists (atPath: String) -> Bool {
+        return fileManagerInstance.fileExists (atPath: atPath)
+    }
+    
+    static public func isDirectory (atPath: String) -> Bool {
+        return fileManagerInstance.isDirectory(atPath: atPath)
+    }
+}
+
+// MARK: FileManager extension
+/// Extension of file manager
+extension FileManager {
+    func isDirectory(atPath: String) -> Bool {
+        var check: ObjCBool = false
+        if fileExists(atPath: atPath, isDirectory: &check) {
+            return check.boolValue
+        } else {
+            return false
+        }
+    }
+}
---
+++ Tests/KnightLifeTests/KnightLifeTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+@testable import KnightLife
+
+final class KnightLifeTests: XCTestCase {
+    func testAllFeatures() throws {
+
+        print ("\u{001b}[37m\u{001b}[41m")
+        print ("""
+               
+               
+                                    Tests are for farmers.
+                               Developers are Knights of the IT!
+               """)
+        print ("\u{001b}[31m                                                 sincerely Bug")
+        print ("\u{001b}[0m")
+        print ("")
+    }
+}

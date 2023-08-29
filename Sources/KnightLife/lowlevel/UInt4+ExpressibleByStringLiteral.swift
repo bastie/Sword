933f850e266300e40bd478cee165e37114209286
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2023-08-29 13:07:34 +0200

    add ExpessibleByStringLiteral protocol

--- Sources/KnightLife/lowlevel/UInt4+ExpressibleByStringLiteral.swift
+++ Sources/KnightLife/lowlevel/UInt4+ExpressibleByStringLiteral.swift
@@ -1,8 +1,15 @@
-//
-//  File.swift
-//  
-//
-//  Created by Sebastian Ritter on 29.08.23.
-//
+/*
+ * SPDX-FileCopyrightText: 2023 - Sebastian Ritter <bastie@users.noreply.github.com>
+ * SPDX-License-Identifier: MIT
+ */
 
-import Foundation
+extension UInt4: ExpressibleByStringLiteral {
+    public typealias StringLiteralType = String
+    
+    public init(stringLiteral value: String) {
+        guard let intValue = UInt8(value, radix: 16), intValue <= 0x0F else {
+            fatalError("Invalid value for UInt4. Value must be between 0 and 15.")
+        }
+        self.value = intValue
+    }
+}
